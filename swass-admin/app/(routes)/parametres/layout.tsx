db94ebefe193376db9539eb249399230e48788f2
nourdine jazi <nourdinejazi2@gmail.com>
2024-10-05 16:15:39 +0100

    Refactor sidebar navigation in SettingsLayout component

--- swass-admin/app/(routes)/parametres/layout.tsx
+++ swass-admin/app/(routes)/parametres/layout.tsx
@@ -11,22 +11,31 @@ let sidebarNavItems = [
     title: "Config Produit",
     href: "/parametres/configproduit",
   },
+];
 
+let sidebarNavItemsSA = [
+  {
+    title: "Profile",
+    href: "/parametres",
+  },
+  {
+    title: "Config Produit",
+    href: "/parametres/configproduit",
+  },
+  {
+    title: "Sécurité",
+    href: "/parametres/securite/adminlist",
+  },
 ];
 
 interface SettingsLayoutProps {
   children: React.ReactNode;
 }
 
-export default async function SettingsLayout({ children }: SettingsLayoutProps) {
+export default async function SettingsLayout({
+  children,
+}: SettingsLayoutProps) {
   const role = await currentRole();
-
-  if (role == "SUPERADMIN") {
-    sidebarNavItems.push({
-      title: "Sécurité",
-      href: "/parametres/securite/adminlist",
-    });
-  }
 
   return (
     <div className="   overflow-auto w-full space-y-6 p-10 pb-16  ">
@@ -36,7 +45,9 @@ export default async function SettingsLayout({ children }: SettingsLayoutProps) 
       />
       <div className="flex flex-col space-y-8 lg:flex-row lg:space-x-12 lg:space-y-0">
         <aside className="-mx-4 lg:w-1/5">
-          <SidebarNav items={sidebarNavItems} />
+          <SidebarNav
+            items={role === "SUPERADMIN" ? sidebarNavItemsSA : sidebarNavItems}
+          />
         </aside>
         <div className="flex-1 lg:max-w-2xl">{children}</div>
       </div>
